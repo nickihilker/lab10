519e0460ea714f52b29d36b2a4a72f6ab3f78cc5
nickihilker <nickihilker@gmail.com>
2014-10-21 14:08:25 -0500

    First Commit

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,3 +14,28 @@
 
 # Minus 5pts if your code has no comments
 # Minus 10pts if you only commit once to github
+
+from Tkinter import *
+root = Tk()
+
+# Create the canvas widget
+drawpad = Canvas(root, width=800,height=600)
+drawpad.grid(row=0, column=1)
+
+
+#roof outline
+
+line1 = drawpad.create_line(50,150,710,150)
+line2 = drawpad.create_line(50,150,90,50)
+line3 = drawpad.create_line(90,50,670,50)
+line4 = drawpad.create_line(670,50,710,150)
+#house outline
+
+house1 = drawpad.create_rectangle(80,150,670,510, fill = 'red')
+
+
+
+
+
+
+root.mainloop()
